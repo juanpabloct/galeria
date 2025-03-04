ea23ac0ca6f001c9cc10481b7a0dc7a7148cda7c
Juan Pablo Castellanos <juanptorres2003@gmail.com>
2025-03-04 23:24:30 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'galeria',
+  webDir: 'dist'
+};
+
+export default config;
