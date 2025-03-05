44652fed63031be1e7c1c5ed1bda5fe92ddcb636
JuanPa-CT <juanptorres2003@gmail.com>
2025-03-04 21:10:00 -0500

    adjust

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,5 +1,11 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import {
+  IonButton,
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar
+} from '@ionic/react';
 import './Tab1.css';
 
 const Tab1: React.FC = () => {
@@ -11,12 +17,13 @@ const Tab1: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        <IonButton
+          onClick={() => {
+            console.log('sfsds');
+          }}
+        >
+          hOLA
+        </IonButton>
       </IonContent>
     </IonPage>
   );
