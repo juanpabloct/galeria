0f9318706c4e9afe4d781a671b887cce3f1fca88
CarlosalbertoCastellanos <Ccastellanosgarzon@gmail.com>
2025-03-10 18:42:15 -0500

    changes to axios operation

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,61 +1,61 @@
-import { useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
 import {
   IonPage,
   IonContent,
   IonInput,
   IonButton,
-  IonTitle
-} from '@ionic/react';
-import { useAuth } from '../context/AuthContext';
-import { server } from '../contants';
-import axios from 'axios';
+  IonTitle,
+} from "@ionic/react";
+import { useAuth } from "../context/AuthContext";
+import { server } from "../contants";
+import axios from "axios";
 
 const SignIn = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const { login } = useAuth();
   const history = useHistory();
 
   const handleSignIn = async () => {
     try {
-      const { data } = await axios(server + 'login/', {
-        method: 'POST',
+      const { data } = await axios(server + "login/", {
+        method: "POST",
         data: {
           email,
-          password
-        }
+          password,
+        },
       });
 
-      localStorage.setItem('token', data.accessToken);
-      localStorage.setItem('userId', data.accessToken);
+      localStorage.setItem("token", data.accessToken);
+      localStorage.setItem("userId", data.accessToken);
       login();
-      history.push('/');
+      history.push("/");
     } catch (err) {
-      alert('Credenciales incorrectas');
+      alert("Credenciales incorrectas " + err);
     }
   };
 
   return (
     <IonPage>
-      <IonContent className='ion-padding'>
-        <IonTitle className='ion-text-center'>Iniciar Sesión</IonTitle>
+      <IonContent className="ion-padding">
+        <IonTitle className="ion-text-center">Iniciar Sesión</IonTitle>
         <IonInput
-          placeholder='Correo electrónico'
+          placeholder="Correo electrónico"
           onIonChange={(e) => setEmail(e.detail.value!)}
         />
         <IonInput
-          type='password'
-          placeholder='Contraseña'
+          type="password"
+          placeholder="Contraseña"
           onIonChange={(e) => setPassword(e.detail.value!)}
         />
-        <IonButton expand='full' onClick={handleSignIn}>
+        <IonButton expand="full" onClick={handleSignIn}>
           Iniciar Sesión
         </IonButton>
 
         {/* Contenedor centrado */}
-        <div style={{ textAlign: 'center', marginTop: '10px' }}>
-          <IonButton fill='clear' onClick={() => history.push('/signup')}>
+        <div style={{ textAlign: "center", marginTop: "10px" }}>
+          <IonButton fill="clear" onClick={() => history.push("/signup")}>
             ¿No tienes cuenta?
           </IonButton>
         </div>
--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,51 +1,51 @@
-import { useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
 import {
   IonPage,
   IonContent,
   IonInput,
   IonButton,
-  IonTitle
-} from '@ionic/react';
-import { useAuth } from '../context/AuthContext';
-import { server } from '../contants';
-import axios from 'axios';
+  IonTitle,
+} from "@ionic/react";
+import { useAuth } from "../context/AuthContext";
+import { server } from "../contants";
+import axios from "axios";
 
 const SignUp = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const { login } = useAuth();
   const history = useHistory();
 
   const handleSignUp = async () => {
     try {
-      const _ = await axios(server + 'login/newUser/', {
-        method: 'POST',
+      const _ = await axios(server + "login/newUser/", {
+        method: "POST",
         data: {
           email,
-          password
-        }
+          password,
+        },
       });
-      history.push('/signin');
+      history.push("/signin");
     } catch (error) {
-      alert('Contraseña debe tener al menos 6 caracteres');
+      alert("Contraseña debe tener al menos 6 caracteres " + error);
     }
   };
 
   return (
     <IonPage>
-      <IonContent className='ion-padding'>
-        <IonTitle className='ion-text-center'>Registro</IonTitle>
+      <IonContent className="ion-padding">
+        <IonTitle className="ion-text-center">Registro</IonTitle>
         <IonInput
-          placeholder='Correo electrónico'
+          placeholder="Correo electrónico"
           onIonChange={(e) => setEmail(e.detail.value!)}
         />
         <IonInput
-          type='password'
-          placeholder='Contraseña'
+          type="password"
+          placeholder="Contraseña"
           onIonChange={(e) => setPassword(e.detail.value!)}
         />
-        <IonButton expand='full' onClick={handleSignUp}>
+        <IonButton expand="full" onClick={handleSignUp}>
           Registrarse
         </IonButton>
       </IonContent>
